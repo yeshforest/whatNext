3374f42807947c6b09e4b96259854bdf49611fbe
haerimRoh <mtxpvksh@gmail.com>
2023-04-28 21:01:58 +0900

    [feat] first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <header>
+      <h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
+          onClick={(event) => {
+            event.preventDefault(); //클릭해도 리로드 x
+            props.onChangeMode();
+          }}
         >
-          Learn React
+          {props.title}
         </a>
-      </header>
+      </h1>
+    </header>
+  );
+}
+const Nav = (props) => {
+  const lis = [];
+  props.topics.map((t) => {
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id); //event객체를 이용해 a태그의 id값을 알아냄
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+};
+const Article = (props) => {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+};
+function App() {
+  const mode = "WELCOME";
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "js is ..." },
+  ];
+  if (mode === "WELCOME") {
+    let content = null;
+  } else if (mode === "READ") {
+  }
+  return (
+    <div>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          alert("HEADER");
+        }}
+      />
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      />
+      <Article title="Welcome" body="Hello, WEB" />
+      <Article title="HI" body="Hello, WEB" />
     </div>
   );
 }
